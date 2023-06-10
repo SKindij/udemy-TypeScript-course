85a8c5e07063c6cc24ddd31a7c3ea0a944b82e24
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-10 16:20:30 +0300

    chore: refactore code for code style consistency

--- training/train.ts
+++ training/train.ts
@@ -1,11 +1,48 @@
-const superset = 'TS is an extension of JS';
-const phrase = 'Modern software development.';
+console.log('Modern software development.');
+console.log('TS is an extension of JS');
 
-console.log(superset);
-console.log(phrase);
+//* define types of variables
+let userName:string = 'Svyn';
+let userAge:number = 42;
+let isAdmin:boolean = true;
+let userCarModel:string = 'Mondeo';
+let useCarYear:number = 2011;
+
+if (isAdmin) {
+  console.log(`Welcome ${userName}-${userAge}.`)
+};
+
+//* define types in functions
+function logCarMsg (name:string, model:string, year:number):void {
+  let carAge:number = 2023 - year;
+  console.log(`${name} has ${carAge} years old ${model}.`)
+};
+
+logCarMsg(userName, userCarModel, useCarYear);
+
+//* define types in objects
+let userData = '{"userNameData": "PiggiBoy", "userCarModelData": "Mustang", "userCarYearData": 2020}';
+let userObj:{
+  userNameData:string, 
+  userCarModelData:string, 
+  userCarYearData:number
+} = JSON.parse(userData);
+
+console.log(userObj);
 
 
-// cd training
-// tsc train.ts
-// node train.js
-// ts-node train.ts
+
+
+
+
+
+
+
+
+
+
+
+//todo cd training
+//todo tsc train.ts
+//todo node train.js
+//todo ts-node train.ts
