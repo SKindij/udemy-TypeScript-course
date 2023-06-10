1eecbf8eecef2b7649df5afbd7b8cb155ebae6f7
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-10 11:49:46 +0300

    init: create initial configuration files

---
+++ training/train.js
@@ -0,0 +1,8 @@
+var superset = 'TS is an extension of JS';
+var phrase = 'Modern software development.';
+console.log(superset);
+console.log(phrase);
+// cd training
+// tsc train.ts
+// node train.js
+// ts-node train.ts
---
+++ training/train.ts
@@ -0,0 +1,11 @@
+const superset = 'TS is an extension of JS';
+const phrase = 'Modern software development.';
+
+console.log(superset);
+console.log(phrase);
+
+
+// cd training
+// tsc train.ts
+// node train.js
+// ts-node train.ts
