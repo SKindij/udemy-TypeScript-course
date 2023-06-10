a4e5a9105ebea0bcc2a02250a1cd0256b22fbc6d
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-10 16:21:23 +0300

    chore: refactore code for code style consistency

--- training/train.js
+++ training/train.js
@@ -1,8 +1,27 @@
-var superset = 'TS is an extension of JS';
-var phrase = 'Modern software development.';
-console.log(superset);
-console.log(phrase);
-// cd training
-// tsc train.ts
-// node train.js
-// ts-node train.ts
+console.log('Modern software development.');
+console.log('TS is an extension of JS');
+//* define types of variables
+var userName = 'Svyn';
+var userAge = 42;
+var isAdmin = true;
+var userCarModel = 'Mondeo';
+var useCarYear = 2011;
+if (isAdmin) {
+    console.log("Welcome ".concat(userName, "-").concat(userAge, "."));
+}
+;
+//* define types in functions
+function logCarMsg(name, model, year) {
+    var carAge = 2023 - year;
+    console.log("".concat(name, " has ").concat(carAge, " years old ").concat(model, "."));
+}
+;
+logCarMsg(userName, userCarModel, useCarYear);
+//* define types in objects
+var userData = '{"userNameData": "PiggiBoy", "userCarModelData": "Mustang", "userCarYearData": 2020}';
+var userObj = JSON.parse(userData);
+console.log(userObj);
+//todo cd training
+//todo tsc train.ts
+//todo node train.js
+//todo ts-node train.ts
