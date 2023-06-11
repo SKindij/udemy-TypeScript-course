17a2caa977dc5bd84c1871ede635023fdec4c2db
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-11 18:15:08 +0300

    refactor: improve code readability

--- training/utilities.ts
+++ training/utilities.ts
@@ -5,34 +5,51 @@ const electricityUserData = {
 };
 
 const waterUserData = {
-	readings: 3,
+	readings: 5,
 	units: "m3",
 };
 
-const elRate = 0.45;
-const wRate = 2;
+const electricityRate:number = 1.45;
+const waterRate:number = 4.70;
 
-const monthPayments = [0, 0]; // [electricity, water]
+const monthPayments:number[] = [0, 0]; //? [electricity, water]
 
-const calculatePayments = (elData, wData, elRate, wRate) => {
-	if (elData.mode === "double" && elData.readings < 50) {
-		monthPayments[0] = elData.readings * elRate * 0.7;
+const calculatePayments = (
+  //first parameter is object that destructures properties readings and mode
+	{ readings, mode }: { readings:number; mode:string },
+	waterData: { readings:number },
+	electricityRate:number,
+	waterRate:number
+  ):void => {
+	if (mode === "double" && readings < 50) {
+		monthPayments[0] = readings * electricityRate * 0.7;
 	} else {
-		monthPayments[0] = elData.readings * elRate;
+		monthPayments[0] = readings * electricityRate;
 	}
 
-	monthPayments[1] = wData.readings * wRate;
+	monthPayments[1] = waterData.readings * waterRate;
 };
 
-calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+calculatePayments(electricityUserData, waterUserData, electricityRate, waterRate);
 
-const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
+const sendInvoice = (
+	[electricity, water]:number[],
+	electricityUserData: { readings:number; units:string },
+	waterUserData: { readings:number; units:string }
+  ):string => {
 	const text = `    Hello!
     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
+    It will cost: ${electricity}€
     
     This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
+    It will cost: ${water}€`;
 
 	return text;
 };
+
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
+
+
+//todo cd training
+//todo ts-node utilities.ts
