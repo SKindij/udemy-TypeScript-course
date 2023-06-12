075388c6355154e8c96ed6a4b86a74190a57fc4b
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-12 23:38:39 +0300

    refactor: simplify complex logic in function

--- practice/modifiers.ts
+++ practice/modifiers.ts
@@ -5,26 +5,30 @@ interface AppUser {
     age: number;
     email?: string;
     //email: string | undefined;
+    motto?: {
+      battlecry: string;
+      slogan: string;
+    };
 };
 
 const someUser:AppUser = {
     login: 'Fater Svyn',
     password: 'sicret',
     age: 42,
+    motto: {
+      battlecry: 'Glory to Ukraine!',
+      slogan: 'Glory to the heroes!'
+    }
 }
 
 const dbName = 'Forest family DB';
 
 function sendUserData(obj:AppUser, db:string, option?:number):void {
-    console.log( obj, db.toLowerCase(), option?.toString() );
-};
+    console.log( obj.motto?.battlecry);
+    console.log( db.toLowerCase(), option?.toString() );
+};  
 
-sendUserData(someUser, dbName, 22);
-
-
-
-
-
+sendUserData(someUser, dbName, 2023);
 
 
 
