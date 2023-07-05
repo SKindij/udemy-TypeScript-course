5dfffddc6095a2be6694f0a531e52fc2c6c76810
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-05 23:07:54 +0300

    chore: add code examples on topic

--- training/type Assertion.ts
+++ training/type Assertion.ts
@@ -1,3 +1,17 @@
-const fetchData = (url:string, method: "GET" | "POST"):void {
+const fetchData = (url:string, method: "GET" | "POST"):void => {
   console.log("Fetched!!!")
+};
+
+const reqOptions = {
+    url: 'https://someurl.com',
+    method: 'GET'
 }
+
+fetchData('quqi.com', 'GET');
+fetchData(reqOptions.url, reqOptions.method as "GET");
+
+
+
+
+
+
