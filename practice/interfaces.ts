0e71fc5f099a6b0edb506966f3754f7b802b79ad
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-09 21:57:32 +0300

    chore: add code examples on topic

--- practice/interfaces.ts
+++ practice/interfaces.ts
@@ -6,7 +6,6 @@
 // }
 
 // Response #1
-
 // {
 //     status: 'available',
 //     data: {
@@ -19,7 +18,6 @@
 // }
 
 // Response #2
-
 // {
 //     status: 'not available',
 //     data: {
@@ -28,11 +26,53 @@
 //     }
 // }
 
-function checkAnimalData(animal) {
-	if ("available") {
-		// Заменить условие!
+type Animal = "cat" | "dog" | "bird";
+
+enum AnimalStatus {
+	Available = "available",
+	NotAvailable = "not available",
+}
+
+interface AnimalData {
+	animal: Animal;
+	breed: string;
+	sterilized?: string;
+}
+// Don't repeat code, use extends
+interface AnimalAvailableData extends AnimalData {
+	location: string;
+	age?: number;
+}
+interface AnimalNotAvailableData {
+	message: string;
+	nextUpdateIn: Date;
+}
+
+// Interfaces should be separated since both responses will have a data field
+// And only by the status it will be difficult to determine the data
+interface AnimalAvailableResponse {
+	status: AnimalStatus.Available;
+	data: AnimalAvailableData;
+}
+interface AnimalNotAvailableResponse {
+	status: AnimalStatus.NotAvailable;
+	data: AnimalNotAvailableData;
+}
+
+type Res = AnimalAvailableResponse | AnimalNotAvailableResponse;
+
+function isAvailable(res:Res): res is AnimalAvailableResponse {
+	if (res.status === AnimalStatus.Available) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function checkAnimalData(animal:Res): AnimalAvailableData | string {
+	if ( isAvailable(animal) ) {
 		return animal.data;
 	} else {
-		return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
+		return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
 	}
 }
