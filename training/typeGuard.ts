9c1093db46981f88e31d558298358b6a0f71905a
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-09 20:29:56 +0300

    chore: refactore code for code style consistency

--- training/typeGuard.ts
+++ training/typeGuard.ts
@@ -1,7 +1,14 @@
+// function isNumber(n: string[] | number | boolean): n is number {
+// 	return typeof n === "number";
+// }
+function isNumber(n: unknown): n is number {
+	return typeof n === "number";
+}
+// use the variable type definition described above
 function printMsg(msg: string[] | number | boolean): void {
-	if (Array.isArray(msg)) {
-		msg.forEach((m) => console.log(m));
-	} else if (isNumber(msg)) {
+	if ( Array.isArray(msg) ) {
+		msg.forEach( (m) => console.log(m) );
+	} else if ( isNumber(msg) ) {
 		console.log(msg);
 	} else {
 		console.log(msg);
@@ -10,13 +17,9 @@ function printMsg(msg: string[] | number | boolean): void {
 }
 
 printMsg(4);
+printMsg(['this', 'is', 'test']);
+printMsg(true);
 
-// function isNumber(n: string[] | number | boolean): n is number {
-// 	return typeof n === "number";
-// }
-function isNumber(n: unknown): n is number {
-	return typeof n === "number";
-}
 
 interface Car {
 	engine: string;
@@ -31,20 +34,23 @@ interface Ship {
 	sail: string;
 }
 
-function repairVehicle(vehicle: Car | Ship) {
-	if (isCar(vehicle)) {
-		vehicle.wheels;
-	} else if (isShip(vehicle)) {
-		vehicle;
-	} else {
-		vehicle;
-	}
-}
-
 function isCar(car: Car | Ship): car is Car {
 	return (car as Car).wheels.number !== undefined;
 }
-
 function isShip(ship: Car | Ship): ship is Ship {
 	return "sail" in ship;
 }
+
+function repairVehicle(vehicle: Car | Ship) {
+	if ( isCar(vehicle) ) {
+		vehicle.wheels;
+	} else if ( isShip(vehicle) ) {
+		vehicle;
+	} else {
+		vehicle; // never
+	}
+}
+
+
+//todo cd training
+//todo ts-node typeGuard.ts
