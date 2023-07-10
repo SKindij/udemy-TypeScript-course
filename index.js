3eb432d3d9e32f63d0823e42a56f858bd712e286
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-10 21:04:55 +0300

    init: add necessary files

---
+++ index.js
@@ -0,0 +1,8 @@
+var boxTS = document.querySelector('.boxTS');
+var inputTS = document.querySelector('input');
+var linkTS = document.querySelector('a');
+console.log(inputTS === null || inputTS === void 0 ? void 0 : inputTS.value);
+if (linkTS) {
+    console.log(linkTS.href);
+}
+//todo tsc index.ts
---
+++ index.ts
@@ -0,0 +1,15 @@
+const boxTS = document.querySelector('.boxTS') as HTMLElement;
+const inputTS = document.querySelector('input');
+const linkTS = document.querySelector('a');
+
+console.log(inputTS?.value);
+
+if (linkTS) {
+    console.log(linkTS.href);
+}
+
+
+
+
+
+//todo tsc index.ts
