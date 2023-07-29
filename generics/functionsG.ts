6def6980315b22534cca1f13688cc05300765f0e
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-29 17:36:15 +0300

    docs: add code examples on topic

--- generics/functionsG.ts
+++ generics/functionsG.ts
@@ -15,7 +15,7 @@ let res1 = processingInputs('eleven', 27);
 let res2 = processingInputs(12.40, 29);
 console.dir( {res1, res2} );
 
-// Generic Types
+// some sample
 interface DataSaver {
   processing: <T>(data:T) => T
 }
@@ -26,7 +26,61 @@ const saver:DataSaver = {
   },
 }
 
+// Generic Interfaces & Types
+interface ProcessFunc {
+  <T>(data:T):T;
+}
+function processer<T>(data:T):T {
+ console.log(data);
+ return data;
+}
+// if you know the type of data you will be working with and you don't plan to change it,
+// you can use "call a specific function directly"
+processer('Сall function directly');
 
+// if you need more flexibility and the ability to work with different types of data, 
+// use generic functions and generic interfaces
+let newProcessFn:ProcessFunc = processer;
+newProcessFn('Make function with Generic Interface');
+
+type Userer<U> = {
+  login: U;
+  age: number;
+};
+const appUser:Userer<string> = {
+  login: 'Olineiro',
+  age: 28
+}
+
+// some helpers
+type OrNull<Type> = Type | null;
+type OneOrMane<Type> = Type | Type[];
+
+const ourData:OneOrMane<number[]> = [14, 19, 26];
+
+// Generic Constraints
+interface ParentsOfUser {
+ mother:string, father:string
+}
+// we use following to have required fields
+interface ComplexUser<ParentsData extends ParentsOfUser> {
+  login:string;
+  age:number;
+  parents:ParentsData;
+}
+
+const complexUserer:ComplexUser<{mother:string, father:string, haveHouse:boolean}> = {
+  login: 'Dondarion',
+  age: 92,
+  parents: {mother: 'Kantra', father: 'Sintro', haveHouse: true}
+}
+
+
+// another interesting example
+const depositMoney = <Q extends number | string>(amount:Q):Q => {
+  console.log(`Bank: ${amount} usd.`);
+  return amount;
+}
 
 
 
