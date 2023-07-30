c91a310f912a703dac3fe7809a2d83d3b9aab8af
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 09:36:26 +0300

    docs: add code examples on topic

--- Info-Resources/UtilityTypes.ts
+++ Info-Resources/UtilityTypes.ts
@@ -1,9 +1,34 @@
 // TypeScript provides several utility types to facilitate common type transformations.
 // These utilities are available globally.
+const arr1:Array<number> = [11, 14, 16];
+const arr2:number[] = [11, 14, 16];
+// it is important to follow the principles of immutability
+const roArr:ReadonlyArray<string> = ['read', 'only', 'array'];
+
+interface IState {
+  data:{},
+  tag?:string
+}
+
+const state1:Partial<IState> = {
+  data: {
+    name: "Fergus"
+  }
+}
+const state2:Required<IState> = {
+    data: {
+      name: "Fergus"
+    },
+    tag: 'magician'
+  }
 
 
+function action(state:Readonly<IState>) {
+  // NO: state.data = 'adc';
+  console.log(state);
+}
 
-
+action(state2);
 
 
 
