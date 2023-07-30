15751e71a88427a457f86ac62cf5ea3acfafd5f5
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 12:26:25 +0300

    init: create an additional file

---
+++ Info-Resources/DifferTypes.ts
@@ -0,0 +1,16 @@
+// condition ? true : false
+// SomeType extends OtherType ? TrueType : FalseType
+type Example = 'Hello' extends 'Hallo' ? string : number;
+
+
+
+
+
+
+
+
+
+
+
+//todo cd Info-Resources
+//todo ts-node DifferTypes.ts
