f8513ab969be8716d40005e7ae9aac4d6ab92ed0
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 09:16:49 +0300

    init: create an additional file

---
+++ Info-Resources/UtilityTypes.ts
@@ -0,0 +1,12 @@
+// TypeScript provides several utility types to facilitate common type transformations.
+// These utilities are available globally.
+
+
+
+
+
+
+
+
+//todo cd Info-Resources
+//todo ts-node UtilityTypes.ts
