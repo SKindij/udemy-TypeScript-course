f60d08bba4439d067f5d2b93ff098bca1baa1adb
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 10:36:18 +0300

    docs: add code examples on topic

--- Info-Resources/UtilityTypes.ts
+++ Info-Resources/UtilityTypes.ts
@@ -6,8 +6,8 @@ const arr2:number[] = [11, 14, 16];
 const roArr:ReadonlyArray<string> = ['read', 'only', 'array'];
 
 interface IState {
-  data:{},
-  tag?:string
+  data:{};
+  tag?:string;
 }
 
 const state1:Partial<IState> = {
@@ -27,9 +27,53 @@ function action(state:Readonly<IState>) {
   // NO: state.data = 'adc';
   console.log(state);
 }
-
 action(state2);
 
+
+interface ICompany {
+  name: string;
+  debts: number;
+  departments:Department;
+}
+
+interface Department {
+  [key:string]:string;
+}
+
+type CompanyKeys = keyof ICompany;
+const keys:CompanyKeys = "debts"; // 'debts' or 'name'
+
+type CompanyNameType = ICompany['name'];
+type CompanyDebtsType = ICompany['debts'];
+type CompanyDepartmentsType = ICompany['departments'];
+type Tester = ICompany [keyof ICompany];
+
+function printDebts<T, K extends keyof T, S extends keyof T>(
+	company:T,	name:K, debts:S) {
+	console.log(`Company ${company[name]}, debts: ${company[debts]}`);
+}
+
+const google:ICompany = {
+	name: "Google",
+	debts: 578000,
+    departments: {
+      admin: 'admin',
+      dev: 'dev',
+      sales: 'sales'
+    }
+};
+const shmoogle:ICompany = {
+	name: "Shmoogle",
+	debts: 193000,
+    departments: {
+      admin: 'admin',
+      dev: 'dev',
+      sales: 'sales'
+    }
+};
+
+printDebts(google, "name", "debts");
+printDebts(shmoogle, 'name', 'debts');
 
 
 
