22b6b03f52ba7534d492884d581592989e811e62
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 21:11:06 +0300

    docs: add code examples on topic

--- Info-Resources/MappedTypes.ts
+++ Info-Resources/MappedTypes.ts
@@ -1,21 +1,27 @@
-type Currencies = {
-  usa: 'usd';
-  china: 'cny';
-  ukraine: 'uah';
-  poland: 'zlt';
+// Mapped Types
+type Keys = 'name' | 'age' | 'role';
+type User = {
+  [K in Keys]: string;
 }
 
+const american:User = {
+  name: 'Alex',
+  age: '31',
+  role: 'cop'
+}
 
-
-
-
-
-
-
-
-
-
-
+// Mapped Types
+type Currencies = {
+    usa: 'usd';
+    china: 'cny';
+    ukraine: 'uah';
+    poland: 'zlt';
+}
+type CreateCustomCurr<Type> = {
+  [P in keyof Type]: string;
+}
+  
+type CustomCurrencies = CreateCustomCurr<Currencies>;
 
 
 
