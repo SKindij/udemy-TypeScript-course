ced39cacca4b7f08677f133b195ab119d34ca93f
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 20:05:45 +0300

    init: create an additional file

---
+++ Info-Resources/MappedTypes.ts
@@ -0,0 +1,26 @@
+type Currencies = {
+  usa: 'usd';
+  china: 'cny';
+  ukraine: 'uah';
+  poland: 'zlt';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//todo cd Info-Resources
+//todo ts-node MappedTypes.ts
