f93cb46f460f0737fd857297b6c30554983e7bd2
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-30 22:07:18 +0300

    init: add necessary files

---
+++ practice/diffTypes.ts
@@ -0,0 +1,87 @@
+// Необхідно типізувати цей великий об'єкт
+// Властивість майбутніх класів повинна бути залежною від класів за типом
+// Властивості exClients та futureClients теж повинні бути залежними від currClients
+// АБО всі три залежать від спільного батька
+
+// Простими словами: при додаванні властивості цільової об'єкт вони повинні бути
+// автоматично додані в залежні (відразу підказка від TS)
+
+const fitnessClubCenter = {
+	clubName: "Fitness club Center",
+	location: "central ave. 45, 5th floor",
+	classes: [
+		{
+			name: "yoga",
+			startsAt: "8:00 AM",
+			duration: 60,
+		},
+		{
+			name: "trx",
+			startsAt: "11:00 AM",
+			duration: 45,
+		},
+		{
+			name: "swimming",
+			startsAt: "3:00 PM",
+			duration: 70,
+		},
+	],
+	futureClasses: [
+		{
+			name: "boxing",
+			willStartsAt: "6:00 PM",
+			duration: 40,
+		},
+		{
+			name: "breath training",
+			willStartsAt: "8:00 PM",
+			duration: 30,
+		},
+	],
+	currClients: [
+		{
+			name: "John Smith",
+			age: "-",
+			gender: "male",
+			timeLeft: "1 month",
+		},
+		{
+			name: "Alise Smith",
+			age: 35,
+			gender: "female",
+			timeLeft: "3 month",
+		},
+		{
+			name: "Ann Sonne",
+			age: 24,
+			gender: "female",
+			timeLeft: "5 month",
+		},
+	],
+	exClients: [
+		{
+			name: "Tom Smooth",
+			age: 50,
+			gender: "male",
+			makeCallFor: new Date("2023-08-12"),
+		},
+	],
+	futureClients: [
+		{
+			name: "Maria",
+			makeCallFor: new Date("2023-07-10"),
+		},
+	],
+};
+
+
+
+
+
+
+
+
+
+
+//todo cd practice
+//todo ts-node warehouses.ts
