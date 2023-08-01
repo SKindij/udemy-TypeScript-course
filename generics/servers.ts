69103f8c99046056b3551671fe7e954adfaaa49e
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-01 21:12:45 +0300

    init: add necessary file

---
+++ generics/servers.ts
@@ -0,0 +1,19 @@
+const jsonTest = '{"name": "Test", "date": "46"}';
+
+const objFromJsonTest = JSON.parse(jsonTest);
+
+// https://jsonplaceholder.typicode.com/
+
+interface ToDo {
+    "userId": number,
+    "id": number,
+    "title": string,
+    "completed": boolean
+  }
+
+
+
+
+
+
+
