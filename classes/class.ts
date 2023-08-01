30ecc9ee0e94a63ff790fa9a63efb4f1334889ad
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-01 22:26:04 +0300

    docs: add code examples on topic

--- classes/class.ts
+++ classes/class.ts
@@ -1,16 +1,26 @@
 // https://www.typescriptlang.org/docs/handbook/compiler-options-in-msbuild.html
-
 class Box {
   width!:number;
   height!:number;
     constructor(width:number, height:number) {
-        this.width = width;
-        this.height = height;
+      this.width = width;
+      this.height = height;
     }
 }
 
 const firstBox = new Box(340, 230);
 console.log(firstBox);
+
+class ColoredBox extends Box {
+  color:string;
+    constructor (width:number, height:number, color:string) {
+      super(width, height);
+      this.color = color;
+    }
+}
+
+const coloredBox = new ColoredBox(200, 150, 'red');
+console.log(coloredBox);
 
 
 
