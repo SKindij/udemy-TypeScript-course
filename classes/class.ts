f5ad997b15774207c4a21181c2a902bf9742f3d2
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-05 21:39:48 +0300

    chore: add code examples on topic

--- classes/class.ts
+++ classes/class.ts
@@ -2,14 +2,45 @@
 class Box {
   width!:number;
   height!:number;
-    constructor(width:number, height:number) {
+  volume!:number|undefined;
+  _content!:string|undefined;
+    constructor(width:number, height:number, volume?:number, content?:string) {
       this.width = width;
       this.height = height;
+      this.volume = volume;
+      this._content = content;
     }
+    calculateVolume():void {
+      if (!this.volume) {
+        this.volume = this.width * this.height;
+        console.log(`Volume of Box = ${this.volume}`);
+      } else {
+        console.log(`Volume of Box = ${this.volume}`);
+      }
+    }
+
+    checkBoxSize(transport:number|number[]):string {
+      if(typeof transport === 'number') {
+        return transport >= this.width ? 'Size is Ok' : 'Width is not Ok';
+      } else {
+        return transport.some(tr => tr >= this.width) ? 'Size is Ok' : 'Width is not Ok';
+      }
+    }
+
+    get boxContent() {
+      return this._content;
+    }
+    set boxContent(value) {
+      this._content = value;
+    }
+
 }
 
 const firstBox = new Box(340, 230);
-console.log(firstBox);
+  console.log(firstBox.calculateVolume());
+  console.log(firstBox.checkBoxSize([240, 410, 190]));
+  console.log(firstBox.checkBoxSize(240));
+  console.log(firstBox.checkBoxSize(380));
 
 class ColoredBox extends Box {
   color:string;
@@ -20,9 +51,9 @@ class ColoredBox extends Box {
 }
 
 const coloredBox = new ColoredBox(200, 150, 'red');
-console.log(coloredBox);
-
-
+  console.log(coloredBox.calculateVolume());
+  console.log(coloredBox.boxContent = 'Test Items');
+  console.log(coloredBox.boxContent);
 
 
 // todo cd classes
