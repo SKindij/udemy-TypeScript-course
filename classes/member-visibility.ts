b49524c54327491df7cf785b05c2148e688629f7
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-06 11:33:32 +0300

    docs: add code examples on topic

--- classes/member-visibility.ts
+++ classes/member-visibility.ts
@@ -1,19 +1,50 @@
 class Player {
-    login:string;
-    password:string;
-    server:string;
+  private login:string;
+  private _password:string;
+  public server:string;
+  protected consent:boolean;
 
-    constructor(login: string, password: string, server: string) {
+    constructor(login:string, password:string, server:string, consent:boolean) {
         this.login = login;
-        this.password = password;
+        this._password = password;
         this.server = server;
+        this.consent = consent;
       }
+  // Метод get для звертання до приватного поля _password
+    get password():string {
+      return this._password;
+    }  
+  // Метод set для встановлення нового значення приватному полю _password 
+    set password(newPass:string) {
+      this._password = newPass;
+    }
+   
 }
 
+// Використання геттерів та сеттерів дозволяє забезпечити додаткові перевірки або логіку при отриманні та зміні значень властивостей.
+// Наприклад, ти можеш додати перевірку довжини паролю в методі set password(newPass: string), 
+// щоб переконатись, що пароль задовольняє певним вимогам, або зашифрувати пароль перед збереженням в _password.
+
+const firstPlayer = new Player('Long Red', 'mypassword', 'Europe', true);
+  console.dir(firstPlayer);
+
+firstPlayer.password = 'anotherPassword';
+  console.dir(firstPlayer);
 
 
-const firstPlayer = new Player('Long Red', 'mypassword', 'Europe');
-console.dir(firstPlayer);
+class CompetitivePlayer extends Player {
+  // опціональна властивість
+  rank?:number;
+
+  isConsented():string {
+    return this.consent ? 'Yes' : "No";
+  }
+}
+
+const secondPlayer = new CompetitivePlayer ('Silver Breez', 'sentFent', 'USA', false);
+  secondPlayer.rank = 2;
+  console.dir(secondPlayer);
+
 
 
 
