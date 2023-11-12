30cdd378be168a4d792c9409269b5c791a8e201b
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-11-12 13:30:40 +0200

    init: initialize new project

--- Decorators/Introduction.ts
+++ Decorators/Introduction.ts
@@ -1,19 +1,31 @@
 // Introduction.ts
 
-function sealed(target) {
-    // do something with 'target' ...
-    
+const myCar = {
+	fuel: "50%",
+	open: true,
+	freeSeats: 3,
+	isOpen() {
+		console.log(`Fuel level: ${this.fuel}.`);
+		return this.open ? "open" : "close";
+	},
 };
 
-function color(value: string) {
-    // this is the decorator factory, it sets up
-    // the returned decorator function
-    return function (target) {
-      // this is the decorator
-      // do something with 'target' and 'value'...
-    };
-};
+// decorators are functions that can modify objects
+function closeCar(car: typeof myCar) {
+	car.open = false;
+	console.log("close car");
+	return car;
+}
 
+function addFuel(car: typeof myCar) {
+	car.fuel = "100%";
+	console.log("add fuel");
+	return car;
+}
+
+addFuel(closeCar(myCar)).isOpen();
+
+// f(x());
 
 
 
